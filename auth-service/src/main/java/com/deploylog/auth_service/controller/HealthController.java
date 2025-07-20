cb5059d061e3e371765ecb3220949724589101c3
Sudhanshu <srsrivastava1926@gmail.com>
2025-07-20 11:43:42 +0530

    Initial commit: Setup Auth-Service skeleton

---
+++ auth-service/src/main/java/com/deploylog/auth_service/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.deploylog.auth_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Auth Service is up!";
+    }
+}
