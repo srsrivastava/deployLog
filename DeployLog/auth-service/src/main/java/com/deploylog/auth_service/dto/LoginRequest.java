b48d3fa7eb3ab18783c4bbdcba1721cf899d00f4
srsrivastava <55092581+srsrivastava@users.noreply.github.com>
2025-07-24 19:08:23 +0530

    Add files via upload

---
+++ DeployLog/auth-service/src/main/java/com/deploylog/auth_service/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.deploylog.auth_service.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 20, message = "Password must be between 8 t0 20 characters")
+    private String password;
+}
